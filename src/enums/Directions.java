9c279deb02b9a51c55f853275535100dba60c1ac
Matias Garin <mgarin@alumno.unlam.edu.ar>
2020-05-30 22:46:35 -0300

    realizamos el main y comenzamos con el ejecutar accion y dejamos preparado para realizar el parser

--- src/enums/Directions.java
+++ src/enums/Directions.java
@@ -27,5 +27,24 @@ public enum Directions {
 	};
 	
 	public abstract String getDescription();
+
+	public static Directions getDirection(String callerObject) {
+		switch (callerObject) {
+		case "norte":
+		case "n":
+			return NORTH;
+		case "sur":
+		case "s":
+			return SOUTH;
+		case "este":
+		case "e":	
+			return EAST;
+		case "oeste": 
+		case "o":
+			return WEST;
+		default:
+			return null;
+		}
+	}
 	
 }
--- src/enums/TriggerAction.java
+++ src/enums/TriggerAction.java
@@ -5,5 +5,13 @@ public enum TriggerAction{
 	PICK_UP,
 	USE,
 	LOOK_AT,
-	TALK_TO
+	TALK_TO;
+	
+	public static TriggerAction getEnum(String action) {
+		try {
+			return valueOf(action);
+		} catch (Exception e) {
+			return null;
+		}
+	}
 }
